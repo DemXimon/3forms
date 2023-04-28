13213ce24a64f1ccd16e58f2cac893e55ec203fc
Skerean <olenev.s82@gmail.com>
2023-04-28 22:33:57 +0300

    yaGay

--- WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -180,7 +180,7 @@ namespace WindowsFormsApp1
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 0);
+            this.button1.Location = new System.Drawing.Point(321, 0);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
